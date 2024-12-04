15f9e5d418670b405714817753b51d09cfa996e1
KawaGregorio <kawagregorio70@gmail.com>
2024-12-04 17:51:09 -0300

    alterações

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function testar(){
     let contOp2=0;
     let contOp3=0;
     let contOp4=0;
-    let contOp5=0;
 
     let radios = document.querySelectorAll('input[type="radio"]')
     console.log(radios)
@@ -21,13 +20,10 @@ function testar(){
             else if(e.value == "op4"){
                 contOp4++
             }
-            else if(e.value == "op5"){
-                contOp5++
-            }
         }
     })
 
-    let resultado = `Op1 = ${contOp1}<br>Op2 = ${contOp2}<br>Op3 = ${contOp3}<br>Op4 = ${contOp4}<br>Op5 = ${contOp5}`;
+    let resultado = `Op1 = ${contOp1}<br>Op2 = ${contOp2}<br>Op3 = ${contOp3}<br>Op4 = ${contOp4}`;
 
     document.getElementById("resultado").innerHTML = resultado;
 
@@ -35,25 +31,27 @@ function testar(){
     localStorage.setItem('op2', contOp2)
     localStorage.setItem('op3', contOp3)
     localStorage.setItem('op4', contOp4)
-    localStorage.setItem('op5', contOp5)
 
     let maior = 0
     let pagina
     if (contOp1>maior) {
         pagina = 'op1'
+        maior = contOp1
     }
     if (contOp2>maior) {
         pagina = 'op2'
+        maior = contOp2
     }
     if (contOp3>maior) {
         pagina = 'op3'
+        maior = contOp3
     }
     if (contOp4>maior) {
         pagina = 'op4'
+        maior = contOp4
     }
-    if (contOp5>maior) {
-        pagina = 'op5'
-    }
+
+    console.log(maior)
 
     setTimeout(() => {
         window.open(pagina+'.html')
