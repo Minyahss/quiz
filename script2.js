19d7a8df6a6f1da7fcd3f39f04b7278df8c224e3
Minyahss <dya.yasmin@gmail.com>
2024-12-16 18:36:11 -0300

    toficano doida

--- script2.js
+++ script2.js
@@ -81,7 +81,7 @@ function maior(retorno) {
 
 function mostrarPagina(pag) {
     setTimeout(() => {
-        window.open('https://minyahss.github.io/quiz/'+ pag + '.html')
+        window.open('https://minyahss.github.io/quiz/outrasPaginas/'+ pag + '.html')
     }, 5000)
 }
 
